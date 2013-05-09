2e663161d0d685533767e0794ac752bd4d473311
Andrew Kesterson <andrew@aklabs.net>
2013-05-09 09:07:36 -0400

    Added jira_stop and jira_start functions for logging work

--- lib/jira.sh
+++ lib/jira.sh
@@ -4,6 +4,46 @@
 
 alias jira="/opt/atlassian-cli/jira.sh --server $JIRA --password \"$JIRA_PASS\" --user \"$JIRA_USER\""
 
+function jira_start()
+{
+    if [ "$1" == "--help" ]; then
+	echo "jira_start <issue key>"
+	echo
+	echo "Starts a work log timer for the given issue. The work log timer"
+	echo "can be stopped (and posted) with jira_stop."
+	return 1
+    fi
+
+    mkdir -p ~/tmp/jira_logs
+    date "+%s" > ~/tmp/jira_logs/$1
+    echo "Started work on $1"
+}
+
+function jira_stop()
+{
+    if [ "$1" == "--help" ]; then
+	echo "jira_stop <issue key> [comment]"
+	echo
+	echo "Stops the work timer for the given issue and posts the log to the ticket."
+	echo "If there is a comment, that will be posted as well."
+    fi
+
+    NOW=$(date "+%s")
+    THEN=$(cat ~/tmp/jira_logs/$1 2>/dev/null || echo $NOW)
+    rm -f ~/tmp/jira_logs/$1
+    SPENT=$(expr $(expr $NOW - $THEN) / 60)
+    if [ $SPENT -lt 1 ]; then
+	echo "Spent less than 1 minutes on $1, will not log work."
+	return 0
+    fi
+
+    echo "Logging ${SPENT} minutes on $1"
+    jira -a addWork \
+	--issue $1 \
+	--timeSpent "${SPENT}m" \
+	--comment "$2"
+}
+
 function jira_filter()
 {
 	if [ "$1" == "--help" ]; then
